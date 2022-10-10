4d3202563fa7ca07abb834690c6289610ed8b1ee
sptr15 <114784261+sptr15@users.noreply.github.com>
2022-10-10 20:30:37 +0700

    Create extend_blog.java

---
+++ extend_blog.java
@@ -0,0 +1,14 @@
+package com.shrj.sriharjoku2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityBlog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog);
+    }
+}
